17cc78c2be73c278c5b0ca0f34c87c5fddd3e136
Larissa Tonetto <larigottschall@hotmail.com>
2022-07-08 11:53:57 -0300

    adição de pré-cadastro

---
+++ cypress/integration/pre-cadastro.spec.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+
+var faker = require('faker');
+
+describe('Funcionalidade Pré-cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('minha-conta')
+
+    });
+
+    it('Deve completar o pré-cadastro com sucesso', () => {
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomeFaker)
+
+        cy.get('#reg_email').type(emailFaker)
+        cy.get('#reg_password').type('!teste@teste$')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain', 'modificados com sucesso')
+
+    });
+
+    /*
+        it('Deve completar o pré cadastro com sucesso usando Comandos Customizados', () => {
+            let emailFaker2 = faker.internet.email()
+            cy.preCadastro (emailFaker2, 'senha!@#forte', 'Larissa', 'Tonetto' )
+    
+            cy.get('.woocommerce-message').should('contain', 'modificados com sucesso')
+    
+        });
+    */
+});
